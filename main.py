1f589c5ea16770fc612aecbd2516f3bb3501a171
zzdd445 <zzdd445@163.com>
2025-09-19 17:13:47 +0800

    [200~移除 allure open，改用 Jenkins 托管报告~

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 def main():
     # 运行测试并生成报告
     pytest.main()
-    os.system("allure generate ./report/allure_raw -o ./report/allure_html --clean && allure open ./report/allure_html")
+    os.system("allure generate ./report/allure_raw -o ./report/allure_html --clean")
 
 if __name__ == "__main__":
     main()
